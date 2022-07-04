c34d399bbc22e9937fbadcda16499eded7b5e040
Tiago Brito <gtiago.brito@hotmail.com>
2022-07-04 15:27:14 -0300

    feat(init): Opção de adicionar tarefas foi finalizada.
    Registrando meu progresso no curso de React

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,14 @@
+import { type } from "os";
 import React from "react";
 import style from "./Botao.module.scss";
 
 class Botao extends React.Component<{
-  children?: React.ReactNode
+  children?: React.ReactNode,
+  type?: "button" | "submit" | "reset" | undefined;
 }> {
   render() {
-    return <button className={style.botao}>{this.props.children}</button>;
+    const {type = "button"} = this.props;
+    return <button type={type} className={style.botao}>{this.props.children}</button>;
   }
 }
 
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,20 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { ITarefa } from "../../types/tarefa";
 import Item from "./Item";
 import style from './Lista.module.scss';
 
 
-function Lista() {
-  let tarefas = [
-    { tarefa: "React", tempo: "02:00:00" },
-    { tarefa: "JavaScript", tempo: "01:00:00" },
-    { tarefa: "TypeScript", tempo: "01:25:00" },
-    { tarefa: "Git", tempo: "00:45:00" },
-  ];
+function Lista({ tarefas }: { tarefas: ITarefa[]}) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        tarefas = [...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00" }]
-      }}>Estudos do dia</h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
