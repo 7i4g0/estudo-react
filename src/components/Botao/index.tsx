7a23f39370e45d3bf96979e7ab02e2ce3edd01e0
Tiago Brito <gtiago.brito@hotmail.com>
2022-07-06 08:36:48 -0300

    refactor: Refatorando Class Componentes
    Refatoração dos componentes Botao e Formulario que eram Class Components.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,15 +2,18 @@ import { type } from "os";
 import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component<{
-  children?: React.ReactNode,
-  type?: "button" | "submit" | "reset" | undefined,
-  onClick?: () => void
-}> {
-  render() {
-    const {type = "button", onClick} = this.props;
-    return <button type={type} onClick={onClick} className={style.botao}>{this.props.children}</button>;
-  }
+interface Props {
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
+}
+
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button type={type} onClick={onClick} className={style.botao}>
+      {children}
+    </button>
+  );
 }
 
 export default Botao;
